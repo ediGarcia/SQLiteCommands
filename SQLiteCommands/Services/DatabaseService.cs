9e691d3d042668bdd5672d9b5843f461d8899bad
avaz <alex.vaz@capgemini.com>
2023-01-18 16:54:04 -0300

    Unit tests for the DatabaseService added

--- SQLiteCommands/Services/DatabaseService.cs
+++ SQLiteCommands/Services/DatabaseService.cs
@@ -8,25 +8,25 @@ public static class DatabaseService
     #region Public Methods
 
     #region OpenConnection
+
     /// <summary>
     /// Opens a connection with a SQLite database file.
     /// </summary>
     /// <param name="path"></param>
-    /// <param name="versionNumber"></param>
     /// <returns></returns>
-    public static SQLiteConnection OpenConnection(string path, int versionNumber = 3) =>
-        new SQLiteConnection(GenerateConnectionString(path, versionNumber)).OpenAndReturn();
+    public static SQLiteConnection OpenConnection(string path) =>
+        new SQLiteConnection(GenerateConnectionString(path)).OpenAndReturn();
     #endregion
 
     #region GenerateConnectionString
+
     /// <summary>
     /// Generates the connection string
     /// </summary>
     /// <param name="path"></param>
-    /// <param name="versionNumber"></param>
     /// <returns></returns>
-    public static string GenerateConnectionString(string path, int versionNumber = 3) =>
-        $"Data Source={path}; Version={versionNumber};";
+    public static string GenerateConnectionString(string path) =>
+        $"Data Source={path}; Version=3;";
     #endregion
 
     #endregion
---
+++ SQLiteCommandsTest/Services/DatabaseServiceTest.cs
@@ -0,0 +1,64 @@
+﻿using HelperMethods;
+using NUnit.Framework;
+using SQLiteCommands.Services;
+using System.Data.SQLite;
+
+namespace SQLiteCommandsTest.Services;
+
+internal class DatabaseServiceTest
+{
+    private const string MockDatabaseDirectoryPath = @"Mock";
+    private const string MockDatabaseFilePath = @"Mock\Test.dat";
+
+    #region SetUps and TearDowns
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp() =>
+        SystemMethods.CreateDirectory(MockDatabaseDirectoryPath);
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown() =>
+        SystemMethods.Delete(MockDatabaseDirectoryPath);
+
+    [TearDown]
+    public void TearDown() =>
+SystemMethods.Delete(MockDatabaseFilePath);
+
+    #endregion
+
+    #region OpenConnection
+
+    [Test]
+    public void DatabaseService_OpenConnection_ShouldCreateADatabaseAndConnectToIt()
+    {
+        Assert.IsFalse(SystemMethods.CheckFileExists(MockDatabaseFilePath));
+
+        // Act
+        using SQLiteConnection connection = DatabaseService.OpenConnection(MockDatabaseFilePath);
+
+        // Assert
+        Assert.AreEqual(SystemMethods.GetFullPath(MockDatabaseFilePath), connection.FileName);
+        Assert.IsTrue(File.Exists(connection.FileName));
+        Assert.AreEqual(0, SystemMethods.GetSizeFromPath(MockDatabaseFilePath));
+        Assert.IsTrue(connection.ServerVersion.StartsWith("3."));
+    }
+
+    #endregion
+
+    #region GenerateConnectionString
+
+    [Test]
+    public void DatabaseService_GenerateConnectionString_ShouldGenerateAConnectionString()
+    {
+        // Arrange
+        const string testPathMock = "testPath";
+
+        // Act
+        string result = DatabaseService.GenerateConnectionString(testPathMock);
+
+        // Assert
+        Assert.AreEqual($"Data Source={testPathMock}; Version=3;", result);
+    }
+
+    #endregion
+}
